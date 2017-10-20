69cff0a5e0c67d0e16a063e59ab9da3d6f560c7e
Rad Mahan (Consultant) <mahan.rad@consultant.volvo.com>
2017-10-20 08:18:33 +0200

    add controller, config and web section

--- src/main/java/com/mahorad/stomp/ctrlr/SoundController.java
+++ src/main/java/com/mahorad/stomp/ctrlr/SoundController.java
@@ -1,7 +1,24 @@
 package com.mahorad.stomp.ctrlr;
 
+import com.mahorad.stomp.model.PlayMessage;
+import com.mahorad.stomp.model.StateMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SoundController {
+
+    @MessageMapping("/sound/play")
+    @SendTo("/dest/sound/play")
+    public PlayMessage play(PlayMessage message) throws Exception {
+        return message;
+    }
+
+    @MessageMapping("/sound/state")
+    @SendTo("/dest/sound/state")
+    public StateMessage state(StateMessage message) throws Exception {
+        return message;
+    }
+
 }
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,48 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/stomp-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/dest/sound/play', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function playNote() {
+    stompClient.send("/app/sound/play", {}, JSON.stringify({'note': $("#note").val()}));
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) { e.preventDefault(); });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { playNote(); });
+});
